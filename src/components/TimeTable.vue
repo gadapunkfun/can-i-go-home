50a1ea2c6a2f60cbd25345dbf93f9d9dee9b907a
gadapunkfun <robert.matijevic@devies.se>
2022-11-11 11:20:57 +0100

    added skeleton loader

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,61 +1,72 @@
 <template>
 	<h3 class="q-my-none q-px-md q-pt-md q-pb-none text-white">{{ currentTime }}</h3>
-	<div class="row justify-between q-px-md">
-		<h3 class="text-positive q-mb-auto">Domkyrkan A</h3>
-		<q-icon class="text-h1 text-positive" name="mdi-bus-stop-uncovered" />
-	</div>
-	<q-card
-		class="q-my-xs"
-		v-for="(departure, i) in sortedDeparturesA.slice(0, 6)"
-		flat
-		square
-		:key="i"
-		:style="'background-color: ' + departure.bgColor"
-	>
+	<q-card v-if="isLoading" flat square class="bg-transparent">
 		<q-card-section horizontal>
 			<div class="row justify-between full-width">
-				<div class="row">
-					<h4 class="q-px-md q-mr-md" :style="'width: 80px; color: ' + departure.fgColor">
-						{{ departure.sname }}
-					</h4>
-					<h4 class="q-my-auto" :style="'color: ' + departure.fgColor">
-						{{ departure.direction }}
-					</h4>
-				</div>
-				<h4 class="q-pr-md" :style="'color: ' + departure.fgColor">
-					{{ getHumanReadableDepartureTime(departure.time) }}
-				</h4>
+				<q-skeleton type="text" height="100px" width="10%" class="text-h4" />
+				<q-skeleton type="text" width="50%" class="text-h4" />
+				<q-skeleton type="text" width="35%" class="text-h4" />
 			</div>
 		</q-card-section>
 	</q-card>
-	<div class="row justify-between q-px-md">
-		<h3 class="text-positive q-mb-auto">Domkyrkan B</h3>
-		<q-icon class="text-h1 text-positive" name="mdi-bus-stop-uncovered" />
-	</div>
-	<q-card
-		class="q-my-xs"
-		v-for="(departure, i) in departureBoardB?.Departure.slice(0, 6)"
-		flat
-		square
-		:key="i"
-		:style="'background-color: ' + departure.bgColor"
-	>
-		<q-card-section horizontal>
-			<div class="row justify-between full-width">
-				<div class="row">
-					<h4 class="q-px-md q-mr-md" :style="'width: 80px; color: ' + departure.fgColor">
-						{{ departure.sname }}
-					</h4>
-					<h4 class="q-my-auto" :style="'color: ' + departure.fgColor">
-						{{ departure.direction }}
+	<template v-else>
+		<div class="row justify-between q-px-md">
+			<h3 class="text-positive q-mb-auto">Domkyrkan A</h3>
+			<q-icon class="text-h1 text-positive" name="mdi-bus-stop-uncovered" />
+		</div>
+		<q-card
+			class="q-my-xs animate__animated animate__flipInX animate__slow"
+			v-for="(departure, i) in departuresA.slice(0, 6)"
+			flat
+			square
+			:key="i"
+			:style="'background-color: ' + departure.bgColor"
+		>
+			<q-card-section horizontal>
+				<div class="row justify-between full-width">
+					<div class="row">
+						<h4 class="q-px-md q-mr-md" :style="'width: 80px; color: ' + departure.fgColor">
+							{{ departure.sname }}
+						</h4>
+						<h4 class="q-my-auto" :style="'color: ' + departure.fgColor">
+							{{ departure.direction }}
+						</h4>
+					</div>
+					<h4 class="q-pr-md" :style="'color: ' + departure.fgColor">
+						{{ getHumanReadableDepartureTime(departure.time) }}
 					</h4>
 				</div>
-				<h4 class="q-pr-md" :style="'color: ' + departure.fgColor">
-					{{ getHumanReadableDepartureTime(departure.time) }}
-				</h4>
-			</div>
-		</q-card-section>
-	</q-card>
+			</q-card-section>
+		</q-card>
+		<div class="row justify-between q-px-md">
+			<h3 class="text-positive q-mb-auto">Domkyrkan B</h3>
+			<q-icon class="text-h1 text-positive" name="mdi-bus-stop-uncovered" />
+		</div>
+		<q-card
+			class="q-my-xs"
+			v-for="(departure, i) in departuresB.slice(0, 6)"
+			flat
+			square
+			:key="i"
+			:style="'background-color: ' + departure.bgColor"
+		>
+			<q-card-section horizontal>
+				<div class="row justify-between full-width">
+					<div class="row">
+						<h4 class="q-px-md q-mr-md" :style="'width: 80px; color: ' + departure.fgColor">
+							{{ departure.sname }}
+						</h4>
+						<h4 class="q-my-auto" :style="'color: ' + departure.fgColor">
+							{{ departure.direction }}
+						</h4>
+					</div>
+					<h4 class="q-pr-md" :style="'color: ' + departure.fgColor">
+						{{ getHumanReadableDepartureTime(departure.time) }}
+					</h4>
+				</div>
+			</q-card-section>
+		</q-card>
+	</template>
 </template>
 
 <script setup lang="ts">
@@ -74,6 +85,7 @@
 	const departuresB = computed(() => {
 		return departures.value.filter(d => d.track === "B");
 	});
+
 	const getHumanReadableDepartureTime = (departureTime: string) => {
 		const currentDate = new Date();
 		const departureDate = new Date();
